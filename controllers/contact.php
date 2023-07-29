7b7c8040cb83c1fba1684cc3aac7b1f9b5ca219a
Sertaç Kocagil <122318400+adra3n@users.noreply.github.com>
2023-07-29 20:49:44 +0300

    controllers

---
+++ controllers/contact.php
@@ -0,0 +1,6 @@
+<?php
+require '../functions.php';
+
+$heading = 'Contact Us';
+
+require "../views/contact.view.php";
--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,6 @@
 <?php
+require '../functions.php';
 
 $heading = "Home";
 
-require "views/index.view.php";
+require "../views/index.view.php";
