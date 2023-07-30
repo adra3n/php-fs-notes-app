bf26aed338ed0972011555d2674c68864931e6e9
adra3n <srtc.kcgl@gmail.com>
2023-07-30 15:16:21 +0300

    database and index

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
-
 <?php
 
 require 'functions.php';
-// require 'router.php';
+//require 'router.php';
+require 'Database.php';
 
-class Database{
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-    public $connection;
-    public function __construct(){
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
-    }
-    public function querry($querry){
-
-        $statement = $this->connection->prepare("select * from posts");
-        $statement->execute();
-
-        $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($posts as $post) {
-            echo "<li>" . $post['title'] . "</li>";
-        }
-    }
-
-
-}
+dd($posts);
