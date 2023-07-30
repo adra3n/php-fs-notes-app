acb26249bf65dc9d4e56828bdc3a47b4770697e1
adra3n <srtc.kcgl@gmail.com>
2023-07-30 15:15:05 +0300

    wip

--- index.php
+++ index.php
@@ -4,17 +4,24 @@
 require 'functions.php';
 // require 'router.php';
 
-// Connect to the MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+class Database{
 
-// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
-$pdo = new PDO($dsn);
+    public $connection;
+    public function __construct(){
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $this->connection = new PDO($dsn);
+    }
+    public function querry($querry){
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
+        $statement = $this->connection->prepare("select * from posts");
+        $statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
+        foreach ($posts as $post) {
+            echo "<li>" . $post['title'] . "</li>";
+        }
+    }
+
+
 }
